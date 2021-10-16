bab28f2adc253284aa3f81d0ee34bc18537624ba
Trex S <rex.sohw@gmail.com>
2021-10-16 18:48:39 +0800

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'class-methods-use-this': 'off',
+    'no-console': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.json'],
+      },
+    },
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  ignorePatterns: ['**/node_modules/*', '**/dist/*'],
+};
---
+++ README.md
@@ -0,0 +1,34 @@
+# Typescript Starter
+
+A minimal Typescript project setup with:
+
+1. Eslint
+2. Prettier.
+
+## Table of Content
+
+- [Typescript Starter](#typescript-starter)
+  - [Table of Content](#table-of-content)
+  - [Prerequisite](#prerequisite)
+  - [Quick Start](#quick-start)
+  - [Package Scripts](#package-scripts)
+
+## Prerequisite
+- Node ^12
+- NPM 6+
+
+## Quick Start
+```sh
+$ npm install
+$ npm run build
+$ npm run start
+```
+
+## Package Scripts
+- Run in development mode: `npm run dev`
+- Run the program in production mode : `npm run start`
+- Build project: `npm run build`
+- Run test suites: `npm run test`
+- Run test suites in watch mode: `npm run test-watch`
+- Check for linting errors: `npm run lint`
+- Format code: `npm run format`
